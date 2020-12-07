6295b740d2081908c7b6c42ee865cabe1e2df051
rrrossettiii <rrrossettiii@gmail.com>
2020-12-07 15:12:57 -0500

    fixed github pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 // Page;
 // =============:
